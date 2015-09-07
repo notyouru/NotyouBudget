d75a8b16fc55d8ae7e7d04ff4a1d0e8979e3fc06
notyou <notyou@ya.ru>
2015-09-08 00:23:00 +0300

    NB-3 Add toolBar

--- app/src/main/java/ru/notyou/notyoubudget/MainActivity.java
+++ app/src/main/java/ru/notyou/notyoubudget/MainActivity.java
@@ -1,17 +1,39 @@
 package ru.notyou.notyoubudget;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
     }
+
+    private void initToolBar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
